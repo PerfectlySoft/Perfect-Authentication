a2db1ced3478a5c7db90c968b7db6c811d5cc27c
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:54:03 -0400

    update minor versions

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
 
-		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0),
+		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 1),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP", majorVersion: 1),
 		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1),
