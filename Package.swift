b2f66b9f26f5db1eac178b157f1f6089c57e9256
Nawar Nory <nawar.a.nouri@gmail.com>
2017-07-13 18:02:38 -0300

    bumped SwiftString to major 2

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
 		]
 
