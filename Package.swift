6e874bc12ad92a4125552e733f257b134f353b08
admin <admin@nextplay.ai>
2017-11-25 14:19:13 -0800

    adjusting swift package and importing relevant commits

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
 			name: "OAuth2"),
 		],
     dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
 		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", majorVersion: 3),
 		]
 
 )
